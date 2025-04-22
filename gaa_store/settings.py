2cd894dbca094c14efa75841da27cd7755d99532
Viki-coding <vikimulhall@gmail.com>
2025-04-22 14:43:49 +0100

    Create base and home page and urls.py, and wire up in settings and main urls

--- gaa_store/settings.py
+++ gaa_store/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
 ]
 
@@ -60,7 +61,11 @@ ROOT_URLCONF = 'gaa_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
