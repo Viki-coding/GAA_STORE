55fb055d669d9ac3b44fefb6a25e0f62c56f1a84
Viki-coding <vikimulhall@gmail.com>
2025-04-22 14:52:25 +0100

    Create static folders for media and css, create base.css file and add hero img

--- gaa_store/settings.py
+++ gaa_store/settings.py
@@ -61,7 +61,6 @@ ROOT_URLCONF = 'gaa_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
             'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
