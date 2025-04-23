a9999fda4f99cbc1f3a24e54516a51196e96a7df
Viki-coding <vikimulhall@gmail.com>
2025-04-23 17:04:27 +0100

    Create a profiles app, add user profile models and add app to settings

---
+++ profiles/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_countries.fields import CountryField
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', 
+        null=True, 
+        blank=True,
+        default='IE'  # ISO 3166-1 alpha-2 code for Ireland)
+    )
+    default_eircode = models.CharField(max_length=10, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.user.email}"
+
+    class Meta:
+        verbose_name = "User Profile"
+        verbose_name_plural = "User Profiles"
+        ordering = ['-joined_date']
+     
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        try:
+            instance.userprofile.save()
+        except Exception as e:
+            # Log the exception or handle it appropriately
+            print(f"Error creating/updating user profile: {e}")
