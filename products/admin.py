59443dcefd708e40e5887362dbc1a90f47c9817a
Viki-coding <vikimulhall@gmail.com>
2025-04-24 21:08:31 +0100

    Add products admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product, Hurley, Grip, Sliotar, Helmet
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Hurley)
+admin.site.register(Grip)
+admin.site.register(Sliotar)
+admin.site.register(Helmet)
