0399ffb99f3b68f0ea94dfa2eabb68863db00ff5
Viki-coding <vikimulhall@gmail.com>
2025-04-25 10:22:33 +0100

    urls.py to products and add url patterns, relates to #4

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import v§iews
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+]
