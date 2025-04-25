a6f8ea4d1560238c01491e6d944517ff24b13ade
Viki-coding <vikimulhall@gmail.com>
2025-04-25 16:16:25 +0100

    Fix hurleys page bug no displaying, include in main urls, path products

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home/index.html'), name='home'),
+    path('products/', include('products.urls'), name='products'),
+
 ]
