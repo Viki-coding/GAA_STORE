f87e87b15ac6350cb211a6b063778f2487f88aae
Viki-coding <vikimulhall@gmail.com>
2025-04-28 15:39:01 +0100

    Fix typos in hurling calculator, amend js to display hurley size only, use <p> for clearer UX, relates to #4

--- static/js/converter.js
+++ static/js/converter.js
@@ -7,7 +7,7 @@
             const cmValue = parseFloat(cmInput.value);
             if (!isNaN(cmValue)) {
                 const inches = Math.floor(cmValue / 2.54); // Convert cm to inches & round down
-                result.textContent = `${cmValue} cm is equal to ${inches} inches. You need a ${inches}-inch hurley.`;
+                result.textContent = `You need a ${inches}-inch hurley.`;
                 result.style.color = '#fff'; 
                 cmInput.value = "";
             } else {
