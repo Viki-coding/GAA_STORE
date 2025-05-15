091dc2cc09dfc36d86d7911fdb22be2fd3f620d5
Viki-coding <vikimulhall@gmail.com>
2025-05-15 14:32:48 +0100

    Remove env.py from version control

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault('MY_ENVIRONMENT_VARIABLE', 'variable_value')
-os.environ["SECRET_KEY"] = (
-	"django-insecure-@dm98x&xsd44xol0&+hmv2577+_2k2gfm81*xtx7u!%&d60#k9"
-)
-os.environ["DEBUG"] = "True"  # Set to "False" in production
