801cb7b25d9e94e5977cf5f88262fffa3a1ebe4b
Viki-coding <vikimulhall@gmail.com>
2025-06-02 11:50:49 +0100

    Update checkout.js with with null chekcs and acessibilty improvments

--- checkout/static/js/checkout.js
+++ checkout/static/js/checkout.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const createProfileCheckbox = document.getElementById('create-profile');
     const passwordFields = document.getElementById('password-fields');
 
-    if (createProfileCheckbox) {
+    if (createProfileCheckbox && passwordFields) {
         createProfileCheckbox.addEventListener('change', function () {
             if (createProfileCheckbox.checked) {
                 passwordFields.style.display = 'block';
+                passwordFields.setAttribute('aria-expanded', 'true');
             } else {
                 passwordFields.style.display = 'none';
+                passwordFields.setAttribute('aria-expanded', 'false');
             }
         });
     }
