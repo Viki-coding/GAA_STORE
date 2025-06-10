5cd3c682d7bba54fd7e1a4ad660049d17e214d20
Viki-coding <vikimulhall@gmail.com>
2025-06-10 06:08:46 +0100

    Add wh

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,7 @@
-import os, stripe
+import os
+import stripe
+import json
+
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
