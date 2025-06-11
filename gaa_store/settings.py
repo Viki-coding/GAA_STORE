d6001d283603076ae184e505f8e157ec0c5d4acf
Viki-coding <vikimulhall@gmail.com>
2025-06-11 12:14:06 +0100

    Update cloudinary settings to fix sliotars not displaying

--- gaa_store/settings.py
+++ gaa_store/settings.py
@@ -231,7 +231,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 WHITENOISE_ALLOW_ALL_ORIGINS = True  
 WHITENOISE_STATIC_PREFIX = STATIC_URL
 WHITENOISE_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Cloudinary settings
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME":    os.environ["CLOUDINARY_CLOUD_NAME"],
+    "API_KEY":       os.environ["CLOUDINARY_API_KEY"],
+    "API_SECRET":    os.environ["CLOUDINARY_API_SECRET"],
+}
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Stripe
