21b1edc2082549a2b5308afc652b87cc934eac76
Viki-coding <vikimulhall@gmail.com>
2025-06-12 09:08:22 +0100

    Fix file path of 404 and 500.html and update views

--- gaa_store/views.py
+++ gaa_store/views.py
@@ -5,11 +5,11 @@ def handle_404(request, exception):
     """
     Custom 404 error handler.
     """
-    return render(request, 'errors/404.html', status=404)
+    return render(request, '404.html', status=404)
 
 
 def handle_500(request):
     """
     Custom 500 error handler.
     """
-    return render(request, 'errors/500.html', status=500)
+    return render(request, '500.html', status=500)
